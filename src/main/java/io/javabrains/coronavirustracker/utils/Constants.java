b503100d1aa04eb92fd16c86b358d07d16cd87d2
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-03-02 21:01:11 +0530

    adding data source constants

---
+++ src/main/java/io/javabrains/coronavirustracker/utils/Constants.java
@@ -0,0 +1,7 @@
+package io.javabrains.coronavirustracker.utils;
+
+public class Constants {
+    final public static String CONFIRMED_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    final public static String DEATHS_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
+    final public static String RECOVERED_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
+}
