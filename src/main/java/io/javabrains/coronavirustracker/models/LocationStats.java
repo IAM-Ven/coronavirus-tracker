efbbd6fe6e04b3bb9d0e0c1071b7306ad200f678
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-03-02 22:36:35 +0530

    added data member for number of deaths and recovered cases

--- src/main/java/io/javabrains/coronavirustracker/models/LocationStats.java
+++ src/main/java/io/javabrains/coronavirustracker/models/LocationStats.java
@@ -4,8 +4,11 @@ import lombok.Data;
 
 @Data
 public class LocationStats {
+    private int id;
     private String state;
     private String country;
     private int latestTotalCases;
     private int diffFromPreviousDay;
+    private int casesRecovered;
+    private int totalDeaths;
 }
