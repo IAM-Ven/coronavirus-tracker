d03754d6b97f9c5611261aa5e886d827b45d9ef8
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-03-07 00:04:34 +0530

    Adding highchart graph for number of reported cases

--- src/main/java/io/javabrains/coronavirustracker/controller/HomeController.java
+++ src/main/java/io/javabrains/coronavirustracker/controller/HomeController.java
@@ -1,6 +1,10 @@
 package io.javabrains.coronavirustracker.controller;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,7 +24,6 @@ public class HomeController {
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
 
-        System.out.println(allStats);
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum());
         model.addAttribute("totalNewCases", allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum());
@@ -30,6 +33,16 @@ public class HomeController {
 
         model.addAttribute("totalDeathCases", allStats.stream().mapToInt(stat -> stat.getTotalDeaths()).sum());
         model.addAttribute("totalNewDeathCases", allStats.stream().mapToInt(stat -> stat.getDeathCasesFromPreviousDay()).sum());
+
+        // top 5 countries with most reported cases
+        List<LocationStats> sorted = allStats.stream().filter(line -> !line.getCountry().contains("China") && !line.getCountry().contains("Others")).sorted(Comparator.comparing(LocationStats::getLatestTotalCases).reversed()).collect(Collectors.toList());
+        Map<String, Integer> barChartData = new LinkedHashMap<>();
+        for (int i = 0; i < 10; i++) {
+            barChartData.put(sorted.get(i).getState() + " (" + sorted.get(i).getCountry() + ")", sorted.get(i).getLatestTotalCases());
+        }
+
+        model.addAttribute("barChartData", barChartData);
+
         return "home";
     }
 
